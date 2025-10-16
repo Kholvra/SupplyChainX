759960612458db79527ce7fe0eac158fca44c56f
kolvra <sabrisirajkholbiabby@gmail.com>
2025-10-16 13:54:48 +0700

    style: button cursor pointer

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,6 +5,6 @@ type buttonProp = {
 
 export default function Button({children,size='py-3 px-4'}:buttonProp){
     return(
-        <button className={`${size} rounded-lg bg-white text-2xl text-blue-900 font-sans font-semibold`}>{children}</button>
+        <button className={`${size} rounded-lg bg-white text-2xl text-blue-900 font-sans font-semibold cursor-pointer`}>{children}</button>
     )
 }
