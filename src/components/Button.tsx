e435ffa15a0d9eb399cfa4ba8177617ab4f9d6c6
kolvra <sabrisirajkholbiabby@gmail.com>
2025-10-16 11:18:00 +0700

    style: bolder button text

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,6 +4,6 @@ type buttonProp = {
 
 export default function Button({children}:buttonProp){
     return(
-        <button className="py-3 px-4 text-2xl rounded-lg bg-white text-blue-900 font-sans">{children}</button>
+        <button className="py-3 px-4 rounded-lg bg-white text-2xl text-blue-900 font-sans font-semibold">{children}</button>
     )
 }
